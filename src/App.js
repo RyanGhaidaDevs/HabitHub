2c589af3293935378d5e64a5147b333516982d9e
Ryan Ghaida <ryaboug@gmail.com>
2020-02-07 12:56:58 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-    
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {},
+    }
+
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+
+  }
+  //switch to componetDidMount after working.
+  componentWillMount(){
+    this.checkLoginStatus();
+  }
+
+  checkLoginStatus(){
+    axios.get("http://localhost:3001/logged_in", {withCredentials: true}).then(response => {
+      if(response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN"){
+        this.setState({
+          loggedInStatus: "LOGGED_IN",
+          user: response.data.user
+        })
+      } else if (!response.data.logged_in && this.state.loggedInStatus === "LOGGED_IN"){
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN",
+          user: {}
+        })
+      }
+    }).catch(error => {
+      console.log("login error:", error)
+    })
+  }
+
+  handleLogin(data){
+    this.setState({
+      loggedInStatus: "LOGGED_IN",
+      user: data.user
+    })
+  }
+
+  handleLogout(){
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    })
+    axios.delete("http://localhost:3001/logged_in", {withCredentials: true}).catch(error => {
+      console.log("logout error:", error)
+    })
+  }
+
+
+
+
+  render(){
+    return (
+      <div className="App">
+      
+      </div>
+    );
+  }
 }
 
-export default App;
+
