ae7787d3692f827cc1f64190dd561b84cdda900c
Ryan Ghaida <ryaboug@gmail.com>
2020-02-10 13:41:40 -0500

    added form for reps reporting

--- src/Home.js
+++ src/Home.js
@@ -12,10 +12,34 @@ class Home extends Component  {
     super(props);
       this.state = {
         reps: 0, 
-        user: {}
+        
       }
 
+  this.handleChange = this.handleChange.bind(this)
   }
+
+  handleChange(event){
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    const {
+      reps
+    } = this.state 
+
+    axios.post("http://localhost:3001/reps", {
+      user: {
+        reps: reps
+      }
+    }, 
+    { withCredentials: true }
+    ).then( response => console.log(response))
+  }
+
 
 
   render(){
@@ -23,6 +47,28 @@ class Home extends Component  {
     return(
       <div>
           <h1> home </h1>
+
+          <form onSubmit={this.handleSubmit} > 
+        <h3> Email </h3> 
+        <input 
+        style={
+          {fontSize: 22,
+            margin: 10,
+          width: 325} 
+        }
+          type="reps" 
+          name="reps" 
+          placeholder="Reps" 
+          value={this.state.reps} 
+          onChange={this.handleChange} 
+          required 
+        />
+        
+      
+
+        </form>
+
+
 
           <Button style={ {fontSize: '14px'} } color="inherit" onClick={this.props.handleLogout}>
             Logout
